113c898cf598b04961350bd99fe9905f29685f2d
Michael Quarne <mikedq1225@gmail.com>
2020-07-14 16:40:12 -0700

    finished project

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-document.getElementById("loan-form").addEventListener("submit", calcResults)
+document.getElementById("loan-form").addEventListener("submit", function (e) {
+	e.preventDefault()
+	// Hide results
+	document.getElementById("results").style.display = "none"
+
+	// Show loading gif
+	document.getElementById("loading").style.display = "block"
+	setTimeout(calcResults, 2000)
+})
 
 function calcResults(e) {
-	e.preventDefault()
 	const amount = document.getElementById("amount")
 	const interest = document.getElementById("interest")
 	const years = document.getElementById("years")
@@ -21,12 +28,16 @@ function calcResults(e) {
 		monthlyPayment.value = monthly.toFixed(2)
 		total.value = (monthly * calculatedPayments).toFixed(2)
 		totalInterest.value = (monthly * calculatedPayments - principle).toFixed(2)
+		document.getElementById("results").style.display = "block" // Show results
+		document.getElementById("loading").style.display = "none" // Hides loading gif
 	} else {
 		showError("Please Check your numbers")
 	}
 }
 
 function showError(error) {
+	document.getElementById("results").style.display = "none" // Hides results during error
+	document.getElementById("loading").style.display = "none" // Hides loading gif during error
 	const errorDiv = document.createElement("div")
 	errorDiv.className = "alert alert-danger" // Adds red error classes
 	errorDiv.appendChild(document.createTextNode(error)) // Creates text node to append to errorDiv
